53ef18be227fe1054bc219700dcd90b46e1f158a
katiaul <camposjoaopedro13@gmail.com>
2024-09-15 00:16:43 -0300

    Stable Update

---
+++ src/main/java/katiaul/util/Json.java
@@ -0,0 +1,17 @@
+package katiaul.util;
+
+import com.google.gson.*;
+
+public class Json {
+    public static final Json instance = new Json();
+
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public <T> T serialize(String json, Class<T> clazz) {
+        return gson.fromJson(json, clazz);
+    }
+
+    public String deserialize(Object object) {
+        return gson.toJson(object);
+    }
+}
